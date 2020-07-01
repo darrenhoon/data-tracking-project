e3f6cc9c2e7aeb2ef0cc6ee7d3762459439d411e
David <Obelisk975@gmail.com>
2020-07-01 21:17:00 +0800

    first commit

---
+++ StartPage.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+
+class Datatrackingapp(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
+        container.pack(side='top',fill='both',expand = True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        frame = StartPage(container,self)
+        self.frames[StartPage] = frame
+        frame.grid(row=0,column=0,sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self,cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Data-Tracker 2020", font=('Arial',12))
+        label.pack(pady=10,padx=10)
+
+        exit_button = tk.Button(self, text='Exit',
+        command = lambda: func('Hope you enjoyed using our app!'))
+        exit_button.pack()
+
+        plot_button = tk.Button(self, text='Start Plot!', 
+        command = lambda: func('Next Page'))
+        plot_button.pack()
+
+def func(param):
+    print(param)
+
+app = Datatrackingapp()
+app.mainloop()
