76e71b2ff9f78458b7ff1df3e9817d49a7653a9b
David <Obelisk975@gmail.com>
2020-07-16 15:25:34 +0800

    Added entrybox to key in values

--- combined.py
+++ combined.py
@@ -124,53 +124,94 @@ class edit_data_page(tk.Frame):
         self.controller.title("Edit Values Page")
 
         text = tk.Label(self,text="Edit Data Page", font=("Times New Roman",14))
-        text.pack()
+        # text.pack()
+        text.place(relx=0.5,anchor='n')
         heading= font.Font(text, text.cget("font"))
         # text.configure(underline = True) #Doesn't underline the entire text
 
         #save button
         ##edited info here will be written on the csv, then the button just goes back to the graphing_page
         #to be updated
+        # save_button = ttk.Button(self, text="Save changes", command = lambda: controller.show_frame(graphing_page))
         save_button = ttk.Button(self, text="Save changes", command = lambda: controller.show_frame(graphing_page))
-        save_button.pack(padx=10,pady=10)
+        # save_button.pack(padx=10,pady=10)
+        save_button.place(relx=0.5,rely=0.1,anchor='n')
 
         #discard button
         discard_button = ttk.Button(self, text="Discard", command = lambda: controller.show_frame(graphing_page))
-        discard_button.pack(padx=10,pady=10)
+        # discard_button.pack(padx=10,pady=10)
+        discard_button.place(relx=0.5,rely=0.15,anchor='n')
 
         #go and edit the csv yourself because you are adding another axis to the plot
         open_csv_button = ttk.Button(self, text="Edit File", command = lambda: controller.open_csv())
-        open_csv_button.pack(padx=10,pady=10)
+        # open_csv_button.pack(padx=10,pady=10)
+        open_csv_button.place(relx=0.5,rely=0.2,anchor='n')
 
         #Display Data
-        tree = ttk.Treeview(self, selectmode = 'extended')
-        tree.pack(side='left',fill=tk.BOTH,expand=True)
-        scrlbar = ttk.Scrollbar(self,orient='vertical',command=tree.yview)
-        scrlbar.pack(side='right',fill='y')
-        tree.configure(yscrollcommand=scrlbar.set)
+        self.tree = Treeview(self, selectmode = 'none')
+        # self.tree.pack(side='left',fill=tk.BOTH,expand=True)
+        self.tree.place(rely=0.25,relx=0.15,relheight=0.5,relwidth=0.7)
+        scrlbar = Scrollbar(self,orient='vertical',command=self.tree.yview)
+        # scrlbar.pack(side='right',fill='y')
+        scrlbar.place(rely=0.25,relx=0.96,relheight=0.5)
+        self.tree.configure(yscrollcommand=scrlbar.set)
 
-        #Data values
-        tree["columns"] = ("1", "2","3")
-        tree['show'] = 'headings'
-        tree.column("1", width=100, anchor='c')
-        tree.column("2", width=100, anchor='c')
-        tree.column("3", width=100, anchor='c')
-        tree.heading("1", text="Index")
-        tree.heading("2", text="Account")
-        tree.heading("3", text="Type")
-        tree.insert("",'end',text="L1",values=("1","Big1","Best"))
-        tree.insert("",'end',text="L2",values=("2","Big2","Best"))
-        tree.insert("",'end',text="L3",values=("3","Big3","Best"))
-        tree.insert("",'end',text="L4",values=("4","Big4","Best"))
-        tree.insert("",'end',text="L5",values=("5","Big5","Best"))
-        tree.insert("",'end',text="L6",values=("6","Big6","Best"))
-        tree.insert("",'end',text="L7",values=("7","Big7","Best"))
-        tree.insert("",'end',text="L8",values=("8","Big8","Best"))
-        tree.insert("",'end',text="L9",values=("9","Big9","Best"))
-        tree.insert("",'end',text="L10",values=("10","Big10","Best"))
-        tree.insert("",'end',text="L11",values=("11","Big11","Best"))
-        tree.insert("",'end',text="L12",values=("12","Big12","Best"))
+        #Tree components
+        self.tree["columns"] = ("1", "2","3")
+        self.tree['show'] = 'headings'
+        self.tree.column("1", width=100, anchor='c')
+        self.tree.column("2", width=100, anchor='c')
+        self.tree.column("3", width=100, anchor='c')
+        self.tree.heading("1", text="Index")
+        self.tree.heading("2", text="Account")
+        self.tree.heading("3", text="Type")
+        self.tree.bind('<ButtonRelease-1>', self.select_item)
 
+        #Sample data values
+        self.tree.insert("",'end',text="L1",values=("1","Big1","Best"))
+        self.tree.insert("",'end',text="L2",values=("2","Big2","Best"))
+        self.tree.insert("",'end',text="L3",values=("3","Big3","Best"))
+        self.tree.insert("",'end',text="L4",values=("4","Big4","Best"))
+        self.tree.insert("",'end',text="L5",values=("5","Big5","Best"))
+        self.tree.insert("",'end',text="L6",values=("6","Big6","Best"))
+        self.tree.insert("",'end',text="L7",values=("7","Big7","Best"))
+        self.tree.insert("",'end',text="L8",values=("8","Big8","Best"))
+        self.tree.insert("",'end',text="L9",values=("9","Big9","Best"))
+        self.tree.insert("",'end',text="L10",values=("10","Big10","Best"))
+        self.tree.insert("",'end',text="L11",values=("11","Big11","Best"))
+        self.tree.insert("",'end',text="L12",values=("12","Big12","Best"))
+        self.tree.insert("",'end',text="L13",values=("13","Big13","Best"))
+        self.tree.insert("",'end',text="L14",values=("14","Big14","Best"))
+        self.tree.insert("",'end',text="L15",values=("15","Big15","Best"))
+        self.tree.insert("",'end',text="L16",values=("16","Big16","Best"))
+
+        #Entry to input values
+        col1 = tk.Label(self,text='Column 1').place(rely=0.8,relx=0.07)
+        col2 = tk.Label(self,text='Column 2').place(rely=0.8,relx=0.38)
+        col3 = tk.Label(self,text='Column 3').place(rely=0.8,relx=0.69)
+
+        col1_entry = tk.Entry(self).place(rely=0.85,relx=0.07)
+        col2_entry = tk.Entry(self).place(rely=0.85,relx=0.38)
+        col3_entry = tk.Entry(self).place(rely=0.85,relx=0.69)
+
+
+    def select_item(self, event):
+        curItem = self.tree.item(self.tree.focus())
+        # curItem = self.tree.(self.tree.focus())
+        col = self.tree.identify_column(event.x)
+        print ('curItem = ', curItem)
+        print ('col = ', col)
+
+        if col == '#0':
+            cell_value = curItem['text']
+        elif col == '#1':
+            cell_value = curItem['values'][0]
+        elif col == '#2':
+            cell_value = curItem['values'][1]
+        elif col == '#3':
+            cell_value = curItem['values'][2]
+
+        print ('cell_value = ', cell_value)
 
 if __name__ == "__main__":
     app = Datatrackingapp()
